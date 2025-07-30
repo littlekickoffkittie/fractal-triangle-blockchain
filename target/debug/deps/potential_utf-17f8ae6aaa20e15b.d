12b9e4aa37e9b75cc49180c9d54f42d2457dcebd
littlekickoffkittie <financingcanfunk@gmail.com>
2025-07-29 21:08:56 -0400

    Add target/debug/deps/potential_utf-17f8ae6aaa20e15b.d 1753837736271

---
+++ target/debug/deps/potential_utf-17f8ae6aaa20e15b.d
@@ -0,0 +1,9 @@
+/project/sandbox/user-workspace/target/debug/deps/potential_utf-17f8ae6aaa20e15b.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/potential_utf-0.1.2/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/potential_utf-0.1.2/src/uchar.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/potential_utf-0.1.2/src/ustr.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libpotential_utf-17f8ae6aaa20e15b.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/potential_utf-0.1.2/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/potential_utf-0.1.2/src/uchar.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/potential_utf-0.1.2/src/ustr.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libpotential_utf-17f8ae6aaa20e15b.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/potential_utf-0.1.2/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/potential_utf-0.1.2/src/uchar.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/potential_utf-0.1.2/src/ustr.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/potential_utf-0.1.2/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/potential_utf-0.1.2/src/uchar.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/potential_utf-0.1.2/src/ustr.rs:
