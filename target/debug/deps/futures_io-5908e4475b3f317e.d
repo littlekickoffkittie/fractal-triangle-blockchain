78536c6864faf0673901acbc6fcdc0b200c47208
littlekickoffkittie <financingcanfunk@gmail.com>
2025-07-29 21:09:21 -0400

    Add target/debug/deps/futures_io-5908e4475b3f317e.d 1753837760460

---
+++ target/debug/deps/futures_io-5908e4475b3f317e.d
@@ -0,0 +1,7 @@
+/project/sandbox/user-workspace/target/debug/deps/futures_io-5908e4475b3f317e.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-io-0.3.31/src/lib.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libfutures_io-5908e4475b3f317e.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-io-0.3.31/src/lib.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libfutures_io-5908e4475b3f317e.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-io-0.3.31/src/lib.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/futures-io-0.3.31/src/lib.rs:
