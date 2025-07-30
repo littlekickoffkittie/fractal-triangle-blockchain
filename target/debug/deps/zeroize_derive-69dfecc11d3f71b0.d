d1b4fc1d84752886000ed02c73ed4e9964a33b91
littlekickoffkittie <financingcanfunk@gmail.com>
2025-07-29 21:07:14 -0400

    Add target/debug/deps/zeroize_derive-69dfecc11d3f71b0.d 1753837633807

---
+++ target/debug/deps/zeroize_derive-69dfecc11d3f71b0.d
@@ -0,0 +1,5 @@
+/project/sandbox/user-workspace/target/debug/deps/zeroize_derive-69dfecc11d3f71b0.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize_derive-1.4.2/src/lib.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libzeroize_derive-69dfecc11d3f71b0.so: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize_derive-1.4.2/src/lib.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zeroize_derive-1.4.2/src/lib.rs:
