f70ed509c0159238c11123a5a5a3d75117ca1f7c
littlekickoffkittie <financingcanfunk@gmail.com>
2025-07-29 21:06:16 -0400

    Add target/debug/deps/libsqlite3_sys-4cb28c21ec1a67b9.d 1753837575569

---
+++ target/debug/deps/libsqlite3_sys-4cb28c21ec1a67b9.d
@@ -0,0 +1,11 @@
+/project/sandbox/user-workspace/target/debug/deps/libsqlite3_sys-4cb28c21ec1a67b9.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libsqlite3-sys-0.25.2/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libsqlite3-sys-0.25.2/src/error.rs /project/sandbox/user-workspace/target/debug/build/libsqlite3-sys-2fca4a30cb0162d0/out/bindgen.rs
+
+/project/sandbox/user-workspace/target/debug/deps/liblibsqlite3_sys-4cb28c21ec1a67b9.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libsqlite3-sys-0.25.2/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libsqlite3-sys-0.25.2/src/error.rs /project/sandbox/user-workspace/target/debug/build/libsqlite3-sys-2fca4a30cb0162d0/out/bindgen.rs
+
+/project/sandbox/user-workspace/target/debug/deps/liblibsqlite3_sys-4cb28c21ec1a67b9.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libsqlite3-sys-0.25.2/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libsqlite3-sys-0.25.2/src/error.rs /project/sandbox/user-workspace/target/debug/build/libsqlite3-sys-2fca4a30cb0162d0/out/bindgen.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libsqlite3-sys-0.25.2/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/libsqlite3-sys-0.25.2/src/error.rs:
+/project/sandbox/user-workspace/target/debug/build/libsqlite3-sys-2fca4a30cb0162d0/out/bindgen.rs:
+
+# env-dep:OUT_DIR=/project/sandbox/user-workspace/target/debug/build/libsqlite3-sys-2fca4a30cb0162d0/out
