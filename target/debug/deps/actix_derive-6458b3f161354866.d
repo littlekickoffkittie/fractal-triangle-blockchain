4b39bfe26e62683df96aa254a00896003c595324
littlekickoffkittie <financingcanfunk@gmail.com>
2025-07-29 21:06:34 -0400

    Add target/debug/deps/actix_derive-6458b3f161354866.d 1753837594318

---
+++ target/debug/deps/actix_derive-6458b3f161354866.d
@@ -0,0 +1,7 @@
+/project/sandbox/user-workspace/target/debug/deps/actix_derive-6458b3f161354866.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/actix_derive-0.6.2/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/actix_derive-0.6.2/src/message.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/actix_derive-0.6.2/src/message_response.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libactix_derive-6458b3f161354866.so: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/actix_derive-0.6.2/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/actix_derive-0.6.2/src/message.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/actix_derive-0.6.2/src/message_response.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/actix_derive-0.6.2/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/actix_derive-0.6.2/src/message.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/actix_derive-0.6.2/src/message_response.rs:
