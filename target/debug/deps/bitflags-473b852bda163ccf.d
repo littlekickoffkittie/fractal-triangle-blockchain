4bea53be92d4e13406cb04833d805fd74559f5c7
littlekickoffkittie <financingcanfunk@gmail.com>
2025-07-29 21:06:09 -0400

    Add target/debug/deps/bitflags-473b852bda163ccf.d 1753837568376

---
+++ target/debug/deps/bitflags-473b852bda163ccf.d
@@ -0,0 +1,7 @@
+/project/sandbox/user-workspace/target/debug/deps/bitflags-473b852bda163ccf.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bitflags-1.3.2/src/lib.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libbitflags-473b852bda163ccf.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bitflags-1.3.2/src/lib.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libbitflags-473b852bda163ccf.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bitflags-1.3.2/src/lib.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/bitflags-1.3.2/src/lib.rs:
