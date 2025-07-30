04b32180c806785395feea6c6fab0dfe36a4328c
littlekickoffkittie <financingcanfunk@gmail.com>
2025-07-29 21:07:20 -0400

    Add target/debug/deps/local_channel-e2a4a7402a1cba29.d 1753837639514

---
+++ target/debug/deps/local_channel-e2a4a7402a1cba29.d
@@ -0,0 +1,8 @@
+/project/sandbox/user-workspace/target/debug/deps/local_channel-e2a4a7402a1cba29.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/local-channel-0.1.5/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/local-channel-0.1.5/src/mpsc.rs
+
+/project/sandbox/user-workspace/target/debug/deps/liblocal_channel-e2a4a7402a1cba29.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/local-channel-0.1.5/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/local-channel-0.1.5/src/mpsc.rs
+
+/project/sandbox/user-workspace/target/debug/deps/liblocal_channel-e2a4a7402a1cba29.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/local-channel-0.1.5/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/local-channel-0.1.5/src/mpsc.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/local-channel-0.1.5/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/local-channel-0.1.5/src/mpsc.rs:
