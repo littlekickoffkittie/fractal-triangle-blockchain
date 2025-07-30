5ee99ec3ad589aaca6569823e356ec1687535f1a
littlekickoffkittie <financingcanfunk@gmail.com>
2025-07-29 21:06:33 -0400

    Add target/debug/deps/subtle-986efd5fcba938b1.d 1753837592564

---
+++ target/debug/deps/subtle-986efd5fcba938b1.d
@@ -0,0 +1,7 @@
+/project/sandbox/user-workspace/target/debug/deps/subtle-986efd5fcba938b1.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/subtle-2.6.1/src/lib.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libsubtle-986efd5fcba938b1.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/subtle-2.6.1/src/lib.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libsubtle-986efd5fcba938b1.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/subtle-2.6.1/src/lib.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/subtle-2.6.1/src/lib.rs:
