7d8737e745623599fd7c3f92afcfc2bb005fbb3a
littlekickoffkittie <financingcanfunk@gmail.com>
2025-07-29 21:07:07 -0400

    Add target/debug/deps/zstd_safe-14cbf14582d72178.d 1753837626911

---
+++ target/debug/deps/zstd_safe-14cbf14582d72178.d
@@ -0,0 +1,8 @@
+/project/sandbox/user-workspace/target/debug/deps/zstd_safe-14cbf14582d72178.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zstd-safe-7.2.4/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zstd-safe-7.2.4/src/constants.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libzstd_safe-14cbf14582d72178.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zstd-safe-7.2.4/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zstd-safe-7.2.4/src/constants.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libzstd_safe-14cbf14582d72178.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zstd-safe-7.2.4/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zstd-safe-7.2.4/src/constants.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zstd-safe-7.2.4/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zstd-safe-7.2.4/src/constants.rs:
