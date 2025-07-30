ac999af07b67083b464a4e15f872261bdd36fa87
littlekickoffkittie <financingcanfunk@gmail.com>
2025-07-29 21:06:18 -0400

    Add target/debug/deps/ed25519-069f838d27fecc79.d 1753837578258

---
+++ target/debug/deps/ed25519-069f838d27fecc79.d
@@ -0,0 +1,8 @@
+/project/sandbox/user-workspace/target/debug/deps/ed25519-069f838d27fecc79.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ed25519-1.5.3/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ed25519-1.5.3/src/../README.md
+
+/project/sandbox/user-workspace/target/debug/deps/libed25519-069f838d27fecc79.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ed25519-1.5.3/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ed25519-1.5.3/src/../README.md
+
+/project/sandbox/user-workspace/target/debug/deps/libed25519-069f838d27fecc79.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ed25519-1.5.3/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ed25519-1.5.3/src/../README.md
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ed25519-1.5.3/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/ed25519-1.5.3/src/../README.md:
