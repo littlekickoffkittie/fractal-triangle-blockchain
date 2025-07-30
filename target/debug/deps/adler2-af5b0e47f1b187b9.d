e98ee15e2f3f11361c6e48313023690708e293e8
littlekickoffkittie <financingcanfunk@gmail.com>
2025-07-29 21:08:11 -0400

    Add target/debug/deps/adler2-af5b0e47f1b187b9.d 1753837690704

---
+++ target/debug/deps/adler2-af5b0e47f1b187b9.d
@@ -0,0 +1,8 @@
+/project/sandbox/user-workspace/target/debug/deps/adler2-af5b0e47f1b187b9.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/algo.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libadler2-af5b0e47f1b187b9.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/algo.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libadler2-af5b0e47f1b187b9.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/algo.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/adler2-2.0.1/src/algo.rs:
