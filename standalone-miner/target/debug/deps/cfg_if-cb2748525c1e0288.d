a57f34feb7c77c37128e6dc2d21d606912f3dc30
littlekickoffkittie <financingcanfunk@gmail.com>
2025-07-29 21:39:13 -0400

    Add standalone-miner/target/debug/deps/cfg_if-cb2748525c1e0288.d 1753839552465

---
+++ standalone-miner/target/debug/deps/cfg_if-cb2748525c1e0288.d
@@ -0,0 +1,7 @@
+/project/sandbox/user-workspace/standalone-miner/target/debug/deps/cfg_if-cb2748525c1e0288.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.1/src/lib.rs
+
+/project/sandbox/user-workspace/standalone-miner/target/debug/deps/libcfg_if-cb2748525c1e0288.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.1/src/lib.rs
+
+/project/sandbox/user-workspace/standalone-miner/target/debug/deps/libcfg_if-cb2748525c1e0288.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.1/src/lib.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-1.0.1/src/lib.rs:
