41e2c44d53d9422f4751554b8da2187bf83f563e
littlekickoffkittie <financingcanfunk@gmail.com>
2025-07-29 21:07:01 -0400

    Add target/debug/deps/language_tags-a6a3c8d7fff1a63c.d 1753837620513

---
+++ target/debug/deps/language_tags-a6a3c8d7fff1a63c.d
@@ -0,0 +1,8 @@
+/project/sandbox/user-workspace/target/debug/deps/language_tags-a6a3c8d7fff1a63c.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/language-tags-0.3.2/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/language-tags-0.3.2/src/iana_registry.rs
+
+/project/sandbox/user-workspace/target/debug/deps/liblanguage_tags-a6a3c8d7fff1a63c.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/language-tags-0.3.2/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/language-tags-0.3.2/src/iana_registry.rs
+
+/project/sandbox/user-workspace/target/debug/deps/liblanguage_tags-a6a3c8d7fff1a63c.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/language-tags-0.3.2/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/language-tags-0.3.2/src/iana_registry.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/language-tags-0.3.2/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/language-tags-0.3.2/src/iana_registry.rs:
