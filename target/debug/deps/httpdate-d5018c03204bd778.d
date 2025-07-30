0d51ca80e1a6e14d57e382bba1674abd8088f9ed
littlekickoffkittie <financingcanfunk@gmail.com>
2025-07-29 21:08:02 -0400

    Add target/debug/deps/httpdate-d5018c03204bd778.d 1753837682073

---
+++ target/debug/deps/httpdate-d5018c03204bd778.d
@@ -0,0 +1,8 @@
+/project/sandbox/user-workspace/target/debug/deps/httpdate-d5018c03204bd778.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/httpdate-1.0.3/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/httpdate-1.0.3/src/date.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libhttpdate-d5018c03204bd778.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/httpdate-1.0.3/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/httpdate-1.0.3/src/date.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libhttpdate-d5018c03204bd778.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/httpdate-1.0.3/src/lib.rs /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/httpdate-1.0.3/src/date.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/httpdate-1.0.3/src/lib.rs:
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/httpdate-1.0.3/src/date.rs:
