dbfe5ffc2c15a70935932b5bac794241b2c3fded
littlekickoffkittie <financingcanfunk@gmail.com>
2025-07-29 21:06:25 -0400

    Add target/debug/deps/cfg_if-f9dbcb08be9ceeee.d 1753837584469

---
+++ target/debug/deps/cfg_if-f9dbcb08be9ceeee.d
@@ -0,0 +1,7 @@
+/project/sandbox/user-workspace/target/debug/deps/cfg_if-f9dbcb08be9ceeee.d: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-0.1.10/src/lib.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libcfg_if-f9dbcb08be9ceeee.rlib: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-0.1.10/src/lib.rs
+
+/project/sandbox/user-workspace/target/debug/deps/libcfg_if-f9dbcb08be9ceeee.rmeta: /root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-0.1.10/src/lib.rs
+
+/root/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cfg-if-0.1.10/src/lib.rs:
